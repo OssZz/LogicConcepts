0597f868d553db326ce6fce63d0094ff6e4c5fb2
OssZz <oscarsalazar1119368@correo.itm.edu.co>
2025-02-17 14:18:05 -0500

    complete 02

---
+++ LogicConcepts/ConsoleApp1/ConsoleExtension.cs
@@ -0,0 +1,14 @@
+﻿
+
+internal class ConsoleExtension
+{
+    internal static object GetInt(string v)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static string GetValidarOptions(string v, List<string> options)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LogicConcepts/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+var answere = string.Empty;
+var options = new List<string> {"s","n" };
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer nuemro");
+    var b = ConsoleExtension.GetInt("Ingrese segundo nuemro");
+    var c = ConsoleExtension.GetInt("Ingrese tercer nuemro");
+
+    if (a > b && a > c)
+    {
+        Console.WriteLine($"El numero mayo es: {a}");
+    }
+    else if (b > a && b > c)
+    {
+        Console.WriteLine($"El numero mayo es: {b}");
+    }
+    else
+    {
+        Console.WriteLine($"El numero mayo es: {c}");
+    }
+
+    do
+    {
+        answere = ConsoleExtension.GetValidarOptions("¿Desea continuar [S]i, [N]o", options);
+    } while (options!.Any(x => x.Equals(answere,StringComparison.CurrentCultureIgnoreCase)));
+
+
+} while (answere! .Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game over");
+  
+
