d018b33c448497778489b2346be7fe292a9c6175
OssZz <oscarsalazar1119368@correo.itm.edu.co>
2025-02-17 13:13:15 -0500

    Complete ejercise 01

---
+++ LogicConcepts/IsNomberOddOrNot/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese un numero o la palabra 'Salir' para salir: ");
+    numberString = Console.ReadLine();
+    if(numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt)) 
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt}, es par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt}, no es par");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"lo que ingresaste: {numberString}, no es un número entero.");
+    }
+
+}while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game over.");
+
